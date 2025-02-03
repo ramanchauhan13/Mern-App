219d14f40a37f80b3200ea2fbfb21e3a51620dbf
Raman Chauhan <118648695+ramanchauhan13@users.noreply.github.com>
2025-02-04 00:05:03 +0530

    Update signup.js

--- frontend/src/pages/signup.js
+++ frontend/src/pages/signup.js
@@ -30,7 +30,7 @@ function Signup() {
             return toastError('All fields are required');
         }
     try{
-        const url = 'http://localhost:8080/auth/signup';
+        const url = 'https://mern-app-api-lyart.vercel.app/auth/signup';
         const response = await fetch(url, {
             method: 'POST',
             headers: {
