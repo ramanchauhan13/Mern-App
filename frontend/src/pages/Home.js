4ebb8564c1ebf2321742edec9fdf01f6fdff53a0
Raman Chauhan <118648695+ramanchauhan13@users.noreply.github.com>
2025-02-04 00:06:14 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -23,7 +23,7 @@ function Home() {
 
   const fetchProducts = async ()=> {
     try{
-      const url = 'http://localhost:8080/products';
+      const url = 'https://mern-app-api-lyart.vercel.app/products';
       const headers = {
         headers : {
           'Authorization' : localStorage.getItem('jwtToken')
