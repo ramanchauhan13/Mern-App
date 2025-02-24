506b5d55a6041ab9bd4698279a495121da039992
Raman Chauhan <118648695+ramanchauhan13@users.noreply.github.com>
2025-02-24 19:34:03 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,11 @@ const PORT = process.env.PORT || 8080;
 app.get('/ping', (req,res)=>{
     res.send("Pong");
 })
+app.use(cors({
+  origin: 'https://mern-app-ui-nine.vercel.app', // Allow only your frontend
+  methods: 'GET,POST,PUT,DELETE',
+  credentials: true // Allow cookies if needed
+}));
 
 app.use(express.json());
 app.use(cors());
